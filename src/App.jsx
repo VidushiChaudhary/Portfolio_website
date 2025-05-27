ae869ff79764f737a6924e72c92560cb475fbd7a
VidushiChaudhary <140183903+VidushiChaudhary@users.noreply.github.com>
2025-05-27 13:46:04 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/education' element={<Education />} />
-        <Route path='/skill' element={<Skills />} />
+        <Route path='/skills' element={<Skills />} />
         <Route path='/project' element={<Project />} />
         <Route path='/contact' element={<Contact />} />
         
